dd17fe2367436d11fdfd9509e9846ea315318b8c
Guite <info@guite.de>
2019-11-28 11:23:19 +0100

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,34 +6,34 @@ BASE_DIR=$INPUT_BASE_DIR
 VERBOSE=${INPUT_VERBOSE:false}
 
 WGET="wget -q"
-if [ $VERBOSE == true ]; then
+if [ $VERBOSE = true ]; then
     WGET="wget"
 fi
 MODULE_PATH="${BASE_DIR}modules/${MODULE_NAME}"
 OUTPUT_PATH="output/${MODULE_NAME}"
 
-if [ $VERBOSE == true ]; then
+if [ $VERBOSE = true ]; then
     echo "Create working directory"
 fi
 mkdir -p "work" && cd "work"
-if [ $VERBOSE == true ]; then
+if [ $VERBOSE = true ]; then
     echo "Download generator"
 fi
 $WGET "https://updates.modulestudio.de/standalone/ModuleStudio-generator.jar"
 
-if [ $VERBOSE == true ]; then
+if [ $VERBOSE = true ]; then
     echo "Fetch model and regenerate it"
 fi
 cp "../${MODULE_PATH}/Resources/docs/model/${MODEL_NAME}" "./${MODEL_NAME}"
 java -jar ModuleStudio-generator.jar ${MODEL_NAME} "output"
 
-if [ $VERBOSE == true ]; then
+if [ $VERBOSE = true ]; then
     echo "Remove unrequired files (marked with generated suffix)"
 fi
 #rm "${OUTPUT_PATH}/Resources/public/images/*.png"
 find "${OUTPUT_PATH}" -type f -name '*.generated.*' -delete
 
-if [ $VERBOSE == true ]; then
+if [ $VERBOSE = true ]; then
     echo "Copy generated module artifacts into the checkout"
 fi
 cp -R "${OUTPUT_PATH}/*" "../${MODULE_PATH}"
